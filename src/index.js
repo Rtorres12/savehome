7b21d1da943a920bea56d63f0956aad632623ad7
Rtorres12 <rtorresa@autonoma.edu.pe>
2021-03-30 19:41:57 -0500

    3er intento

--- src/index.js
+++ src/index.js
@@ -34,11 +34,11 @@ app.use(multer({
 app.use(morgan('dev'));
 app.use(myConnection(mysql, {
     
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    port: '3306'
+    host: 'database-server',
+    user: 'savehome',
+    password: 'Save2Home$',
+    database: 'savehomebd',
+    port: '3306' 
 },'single'));
 app.use(express.urlencoded({extended: false}));
 
